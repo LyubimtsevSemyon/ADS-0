a9c5cbb79793d35659a77413a8faec5d5bc918d6
LyubimtsevSemyon <99214966+LyubimtsevSemyon@users.noreply.github.com>
2022-02-13 19:06:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,4 +2,21 @@
 
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
+    int c = 0;
+bool f = 0;
+while (f == 0) {
+if (a >= b)
+a = a-b;
+else
+a = b-a;
+if (a%b == 0) {
+c = b;
+f = 1;
+}
+if (b%a == 0) {
+c = a;
+f = 1;
+}
+}
+return c;
 }
